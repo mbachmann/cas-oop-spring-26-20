fe6352360465c9bec8c60f33ddfc8e06adff95a4
mbachmann <m.bachmann@united.ch>
2026-05-15 09:46:42 +0200

    migrate to spring boot 4.0.6 - java 25 migrate to bootstrap 5.x

--- src/main/java/com/example/demoinitial/web/UserController.java
+++ src/main/java/com/example/demoinitial/web/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,12 +36,13 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String showSignUpForm(User user) {
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
         return "add-user";
     }
 
     @PostMapping("/adduser")
-    public String addUser(@Valid User user, BindingResult result, Model model) throws Exception {
+    public String addUser(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) throws Exception {
         if (result.hasErrors()) {
             return "add-user";
         }
