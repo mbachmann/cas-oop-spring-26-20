5ba6a143619b0a29116d1a7e51133469f3773f62
mbachmann <m.bachmann@united.ch>
2026-05-19 08:54:04 +0200

    get the logged in user in auditor

--- src/test/java/com/example/demoinitial/service/UserDetailsServiceTest.java
+++ src/test/java/com/example/demoinitial/service/UserDetailsServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class AppUserDetailsServiceTest {
+public class UserDetailsServiceTest {
 
     @Autowired
     UserDetailsServiceImpl userDetailsService;
